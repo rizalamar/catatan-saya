763bfb434e21b0ef0800bc77346ff87cddddfee4
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-03-09 22:52:23 +0700

    catatan saya app

---
+++ README.md
@@ -0,0 +1,42 @@
+# 📝 catatan-saya
+
+Aplikasi catatan sederhana yang dibangun sebagai latihan pertama menggunakan **React + TypeScript + Tailwind CSS**.
+
+![screenshot](./src/assets/screenshot.png)
+
+---
+
+## ✨ Fitur
+
+-   Tambah catatan baru dengan judul dan isi
+-   Hapus catatan
+-   Search/filter catatan berdasarkan judul (real-time)
+-   Menampilkan total catatan yang sedang ditampilkan
+
+---
+
+## 🚀 Cara Install & Run
+
+```bash
+# Clone repo
+git clone https://github.com/username/catatan-saya.git
+cd catatan-saya
+
+# Install dependencies
+npm install
+
+# Jalankan dev server
+npm run dev
+```
+
+Buka [http://localhost:5173](http://localhost:5173) di browser.
+
+---
+
+## 🛠 Tech Stack
+
+-   [React](https://react.dev/) + [TypeScript](https://www.typescriptlang.org/)
+-   [Tailwind CSS](https://tailwindcss.com/)
+-   [Vite](https://vitejs.dev/)
+
+---
---
+++ src/data-dummy.ts
@@ -0,0 +1,68 @@
+export type Note = {
+	id: number;
+	title: string;
+	body: string;
+};
+
+export const dummyNotes: Note[] = [
+	{
+		"id": 1,
+		"title": "mus. Proin",
+		"body": "Aliquam nec enim. Nunc ut erat. Sed nunc est, mollis non, cursus non, egestas a, dui. Cras",
+	},
+	{
+		"id": 2,
+		"title": "risus. Nunc",
+		"body": "eget tincidunt dui augue eu tellus. Phasellus elit pede, malesuada vel, venenatis vel,",
+	},
+	{
+		"id": 3,
+		"title": "nisi dictum augue",
+		"body": "lorem ipsum sodales purus, in molestie tortor nibh sit amet orci. Ut sagittis lobortis mauris. Suspendisse aliquet",
+	},
+	{
+		"id": 4,
+		"title": "nec urna",
+		"body": "lorem ac risus. Morbi metus. Vivamus euismod urna. Nullam lobortis quam a felis ullamcorper viverra. Maecenas iaculis",
+	},
+	{
+		"id": 5,
+		"title": "interdum. Nunc sollicitudin",
+		"body": "at augue id ante dictum cursus. Nunc mauris elit, dictum eu, eleifend nec, malesuada ut, sem. Nulla",
+	},
+	{
+		"id": 6,
+		"title": "Mauris magna. Duis",
+		"body": "gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna. Cras convallis convallis dolor. Quisque tincidunt",
+	},
+	{
+		"id": 7,
+		"title": "lectus rutrum urna,",
+		"body": "dignissim magna a tortor. Nunc commodo auctor velit. Aliquam nisl. Nulla",
+	},
+	{
+		"id": 8,
+		"title": "cursus a, enim.",
+		"body": "in, hendrerit consectetuer, cursus et, magna. Praesent interdum ligula eu enim.",
+	},
+	{
+		"id": 9,
+		"title": "Lorem ipsum dolor",
+		"body": "quis, tristique ac, eleifend vitae, erat. Vivamus nisi. Mauris nulla. Integer urna. Vivamus molestie dapibus ligula. Aliquam",
+	},
+	{
+		"id": 10,
+		"title": "nec, eleifend",
+		"body": "Nunc lectus pede, ultrices a, auctor non, feugiat nec, diam. Duis",
+	},
+	{
+		"id": 11,
+		"title": "risus a",
+		"body": "mi. Aliquam gravida mauris ut mi. Duis risus odio, auctor vitae, aliquet",
+	},
+	{
+		"id": 12,
+		"title": "ornare, lectus ante",
+		"body": "quis lectus. Nullam suscipit, est ac facilisis facilisis, magna tellus faucibus leo, in lobortis tellus justo sit amet",
+	},
+];
