4dcb1852ef878b82cc3557eb13605d3a762e2f84
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-03-12 22:24:30 +0700

    add custom hook: useNotes()

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,21 @@
-import { useEffect, useState } from "react";
-import type { Note } from "./data-dummy";
 import Header from "./components/Header";
 import NoteForm from "./components/NoteForm";
 import SearchForm from "./components/SearchForm";
 import NoteList from "./components/NoteList";
+import { useNotes } from "./hooks/useNotes";
 
 function NoteApp() {
-	const [notes, setNotes] = useState<Note[]>(() => {
-		const saved = localStorage.getItem("notes");
-		return saved ? JSON.parse(saved) : [];
-	});
-	const [title, setTitle] = useState<string>("");
-	const [body, setBody] = useState<string>("");
-	const [querySearch, setQuerySearch] = useState<string>("");
-
-	useEffect(() => {
-		localStorage.setItem("notes", JSON.stringify(notes));
-	}, [notes]);
-
-	const filteredNotes = notes.filter((note) => note.title.toLowerCase().includes(querySearch.toLowerCase()));
-
-	function handleAddNote() {
-		if (title.trim() === "") return;
-
-		const newNote: Note = {
-			id: Date.now(),
-			title: title,
-			body: body,
-		};
-
-		setNotes([...notes, newNote]);
-		setTitle("");
-		setBody("");
-	}
-
-	function handleDeleteNote(id: number) {
-		const note = notes.filter((note) => note.id !== id);
-		setNotes(note);
-	}
-
+	const {
+		title,
+		setTitle,
+		body,
+		setBody,
+		querySearch,
+		setQuerySearch,
+		filteredNotes,
+		handleAddNote,
+		handleDeleteNote,
+	} = useNotes();
 	return (
 		<div className="min-h-screen bg-stone-950 text-stone-100 font-mono p-8 flex flex-col items-center">
 			{/* Header */}
